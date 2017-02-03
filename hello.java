0cb5e02cd0e0046d018d4f9c81c06e3c6a11b969
mayur lokare <mayur.lokare24@gmail.com>
2017-02-03 16:55:38 +0530

    adding one more line to check merge command

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello {
 public static void main(String args[]){
 System.out.println("This is hello class");
 System.out.println("adding one more statement");
+System.out.println("THIS IS NEW STATEMENT ADDED");
 }
 }
