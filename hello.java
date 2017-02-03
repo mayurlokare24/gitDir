a06eab7256bdd7e790c172607833b6d552cb6ee7
mayur lokare <mayur.lokare24@gmail.com>
2017-02-03 16:58:10 +0530

    adding the new line to check the new branch merge commanf

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public static void main(String args[]){
 System.out.println("This is hello class");
 System.out.println("adding one more statement");
 System.out.println("THIS IS NEW STATEMENT ADDED");
+System.out.println("ABC statement");
 }
 }
